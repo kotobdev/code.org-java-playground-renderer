99ebd0900f35338811ecc3b3ccff3ebd3ae0859e
KOTOB <59124164+kotobdev@users.noreply.github.com>
2022-01-12 08:22:06 -0800

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,35 @@
+import org.code.playground.*;
+import org.code.media.*;
+
+public final class Game {
+
+  /* Use this method to setup the display (show the images).
+   * Calling this method shows the images on the screen so
+   * the user can begin playing the game.
+   */
+  public static void play() {
+    
+  }
+
+  /* Use this method to check whether the player's move
+   * is valid. You may need to add parameters based on
+   * the rules of your game.
+   */
+  public static boolean isValidMove() {
+    
+    return false;
+  }
+
+  /* Use this method to update the score based on the
+   * results of the isValidMove() method. You may need
+   * add parameters based on the rules of your game.
+   */
+  public static void updateScore() {
+    
+  }
+
+  // Use this method to check if the game is over.
+  public static void gameOver() {
+    
+  }
+}
